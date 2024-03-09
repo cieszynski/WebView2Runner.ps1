93872db8138889799fc1a6ce4d7339c443c8c41f
cieszynski <admin@cieszynski.de>
2024-03-09 18:24:30 +0100

    initial upload

--- WebView2Runner.ps1
+++ WebView2Runner.ps1
@@ -1,13 +1,42 @@
-Set-ExecutionPolicy -ExecutionPolicy bypass -Scope Process
+<#
+    WebView2Runner.ps1
 
+    Copyright (C) 2024 Stephan Cieszynski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#>
+
+param(
+    [String] $Url = "https://assets/index.html"
+)
+
+Set-ExecutionPolicy -ExecutionPolicy bypass -Scope Process
 
 Add-Type -AssemblyName System.Windows.Forms
 
+# Make sure you distribute following files
 Add-Type -Path "$PSScriptRoot\Microsoft.Web.WebView2.WinForms.dll"
 Add-Type -Path "$PSScriptRoot\Microsoft.Web.WebView2.Core.dll"
 
+
+
 $script = @"
- const runner = window.chrome.webview;   
+const runner = window.chrome.webview;   
+
+window.onbeforeclose = (event) => {
+alert(event)
+}
 "@
 
 $window = New-Object -TypeName System.Windows.Forms.Form -Property @{
@@ -34,16 +63,17 @@ $webview.Add_CoreWebView2InitializationCompleted({
         $event.Cancel = $true
         $webview.CoreWebView2.ExecuteScriptAsync('window.close()')
         Write-Host "closing"
+        #quit
     })
 
     # CoreWebView2HostResourceAccessKind.Allow    1
     # CoreWebView2HostResourceAccessKind.Deny     0
     # CoreWebView2HostResourceAccessKind.DenyCors 2
     $webview.CoreWebView2.SetVirtualHostNameToFolderMapping('assets', "$PSScriptRoot\assets", 1)
-   # $webview.CoreWebView2.Add_WindowCloseRequested({
-    #    Write-Host "closing2"
-    #    quit
-   # })
+    $webview.CoreWebView2.Add_WindowCloseRequested({
+        Write-Host "closing2"
+        quit
+    })
 
     $webview.CoreWebView2.OpenDevToolsWindow()
     $webview.CoreWebView2.Settings.AreDefaultContextMenusEnabled = $false
@@ -55,7 +85,7 @@ $webview.Add_CoreWebView2InitializationCompleted({
 
 $webview.Size = $window.Size
 $webview.Dock = "Fill"
-    $webview.Source = "https://assets/index.html"
+    $webview.Source = $Url
 $window.Controls.Add($webview)
 
 $window.ShowDialog() | Out-Null
